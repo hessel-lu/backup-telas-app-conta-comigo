d38c55a43cd6eeecedc3f0a4013ea6b611c6d76e
Lucas Hessel <lucas.hessel@sptech.school>
2023-09-05 15:56:44 -0300

    adicionado backup das telas feitas para o projeto

---
+++ ContaComigoTelasHessel/app/src/main/java/br/contacomigo_telashessel/Activity_listagem_itens.kt
@@ -0,0 +1,11 @@
+package br.contacomigo_telashessel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Activity_listagem_itens : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_listagem_itens)
+    }
+}
