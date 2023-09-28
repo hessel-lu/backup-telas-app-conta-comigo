183d0f4a40a5b08c40a22e9217a23324f072ad7f
Lucas Hessel <lucas.hessel@sptech.school>
2023-09-28 13:51:02 -0300

    implementando tela divisao igual - etapa final

---
+++ ContaComigoTelasHessel/app/src/main/java/br/contacomigo_telashessel/ActivityDivisaoIgualEtapaFinal.kt
@@ -0,0 +1,11 @@
+package br.contacomigo_telashessel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityDivisaoIgualEtapaFinal : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_divisao_igual_etapa_final)
+    }
+}
